f129be21b81666ce857de29dbeed4dd6b5c66534
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2016-01-22 10:16:12 +0200

    Pagination to use WP def value

--- function/dev-utils.php
+++ function/dev-utils.php
@@ -1,21 +1,21 @@
 <?php
 
-function get_pagelet($slug, $name=null) 
+function get_pagelet($slug, $name=null)
 {
 	get_template_part('pagelet/' . $slug, $name);
 }
 
-function get_util($slug, $name=null) 
+function get_util($slug, $name=null)
 {
 	get_template_part('util/' . $slug, $name);
 }
 
 // Great site for generating queries: http://generatewp.com/wp_query/
-function do_loop($fn, $query_args=null, $enable_pagination=true, &$fn_args=null) 
+function do_loop($fn, $query_args=null, $enable_pagination=true, &$fn_args=null)
 {
 	global $wp_query;
 	$query = ($query_args == null ? $wp_query : new WP_Query($query_args));
-	
+
 	if ($query->have_posts()) {
 		$post_count = $query->post_count;
 		$index = 0;
@@ -28,8 +28,8 @@ function do_loop($fn, $query_args=null, $enable_pagination=true, &$fn_args=null)
 
 		<?php if ($enable_pagination) : ?>
 		<div class="pagination aligncontainer">
-			<div class="nav-next alignleft"><?php previous_posts_link( _text('pagination-next') ); ?></div>
-			<div class="nav-previous alignright"><?php next_posts_link( _text('pagination-prev') ); ?></div>
+			<div class="nav-next alignleft"><?php previous_posts_link(); ?></div>
+			<div class="nav-previous alignright"><?php next_posts_link(); ?></div>
 		</div>
 		<?php endif; ?>
 

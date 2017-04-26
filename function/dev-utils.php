657507e26f1588a055b7b8e8fff3155633c5bd30
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2017-04-26 16:35:06 +0300

    Nested menu items function

--- function/dev-utils.php
+++ function/dev-utils.php
@@ -53,4 +53,37 @@ function do_loop($fn, $query_args=null, $enable_pagination=true, &$fn_args=null)
 	}
 }
 
+/**
+ * Returns nested object array of menu. All submenu items are in $menu->children 
+ * array in parent menu item.
+ *
+ * @param current_menu - name of menu or menu location
+ * @return array of nested menu items or false if none found
+ */
+function wp_get_menu_items_nested($current_menu)
+{
+    $array_menu = wp_get_nav_menu_items($current_menu);
+	// If no menu found with $current_menu, try to find menu in that named location
+	if ( ! $array_menu && ( $locations = get_nav_menu_locations() ) && isset( $locations[ $current_menu ] ) ) {
+		$array_menu = wp_get_nav_menu_items( $locations[ $current_menu ] );
+	}
+	if (!$array_menu) {
+		return false;
+	}
+    $menu = array();
+    foreach ($array_menu as $m) {
+        if (empty($m->menu_item_parent)) {
+			$m->children = array();
+			$menu[$m->ID] = $m;
+        }
+    }
+    $submenu = array();
+    foreach ($array_menu as $m) {
+        if ($m->menu_item_parent) {
+            $menu[$m->menu_item_parent]->children[$m->ID] = $m;
+        }
+    }
+    return $menu;
+}
+
 ?>
